49f4765c503a46d0ac0cbc23ffef45ab5ac80032
crisanbe <55001756+crisanbe@users.noreply.github.com>
2022-02-05 15:19:38 -0500

    (sESION 4):Verificar que una clase no se ejecute

--- app/src/test/java/com/cursosandroidant/calculator/CalculatorUtilsMockTest.kt
+++ app/src/test/java/com/cursosandroidant/calculator/CalculatorUtilsMockTest.kt
@@ -28,10 +28,10 @@ class CalculatorUtilsMockTest {
         val operation = "-5*2.5"
         val isFromResolve = true
         calculatorUtils.checkOrResolve(operation, isFromResolve)
-        Mockito.verify(operations).tryResolve(operation,isFromResolve,listener)
+        Mockito.verify(operations).tryResolve(operation,isFromResolve,listener)//verificar que se cumpla la operaciones segun con calculatorUtils
     }
 
-    @Test
+    @Test//valida el operador menos despues del mas
     fun calc_callAddOperator_validSub_noReturn(){
         val operator = "-"
         val operation = "4+"//4+-3  ejemplo de validacion operacion matematica validad
@@ -42,4 +42,25 @@ class CalculatorUtilsMockTest {
         assertTrue(isCorrect)
     }
 
+    @Test//Metodo invalido no debe retornar otro operador menos
+    fun calc_callAddOperator_invalidSub_noReturn(){
+        val operator = "-"
+        val operation = "4+-"//4+-3  ejemplo de validacion operacion matematica validad
+        var isCorrect = false
+        calculatorUtils.addOperator(operator, operation){
+            isCorrect = true
+        }
+        assertFalse(isCorrect)
+    }
+
+    @Test//adiccionar un punto 3*2. correcto
+    fun calc_callAddPoint_firstPoint_no_Return(){
+        val operator = "3*2"
+        var isCorret = false
+        calculatorUtils.addPoint(operator){
+            isCorret = true
+        }
+        assertTrue(isCorret)
+        Mockito.verifyNoInteractions(operations)//Verificar que nunca he llamado a operations En caso de que no tenga un punto o que lo tenga
+    }
 }
